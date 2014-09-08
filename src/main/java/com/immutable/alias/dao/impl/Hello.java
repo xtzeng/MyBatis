2217e04b5d96e62b81c32342526818f463551b4b
xt <xiaoti_zeng@126.com>
2014-09-09 07:59:12 +0800

    add hello

--- src/main/java/com/immutable/alias/dao/impl/Hello.java
+++ src/main/java/com/immutable/alias/dao/impl/Hello.java
@@ -1,11 +1,12 @@
 package com.immutable.alias.dao.impl;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 
-public class Hello  {
+public class Hello  extends SqlSessionDaoSupport{
 	private SqlSessionTemplate sqlSessionTemplate;
 	
 	public void test() {
-		
+		this.getSqlSession().selectOne("statement");
 	}
 }
